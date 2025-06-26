8ade903981d5524435b5322c6749107e683a6b88
Carlos Cruz <cruzgarciacarlos87@outlook.com>
2025-06-26 11:43:23 -0600

    Zero to master code interview initial commit

--- DataStructuresAndAlgorithms/Playground/Program.cs
+++ DataStructuresAndAlgorithms/Playground/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Playground
+﻿using ZTMCodeInterview;
+
+namespace Playground
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var bigO = new BigO();
+            bigO.MeasureTimeExecution();
         }
     }
 }
---
+++ DataStructuresAndAlgorithms/ZTMCodeInterview/BigO.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ZTMCodeInterview
+{
+    public class BigO
+    {
+        public void MeasureTimeExecution()
+        {
+            long startTime = Stopwatch.GetTimestamp();
+            // Your code: Doing something e.g
+            Thread.Sleep(3000);
+            TimeSpan elapsedTime = Stopwatch.GetElapsedTime(startTime);
+            Console.WriteLine(elapsedTime.TotalSeconds);
+        }
+    }
+}
