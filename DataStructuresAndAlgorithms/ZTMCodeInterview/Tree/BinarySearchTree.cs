34e7770454bc7d051fa327cf0c4a7270bfce2f8e
Carlos Cruz <cruzgarciacarlos87@outlook.com>
2025-07-03 16:37:45 -0600

    binary search tree

---
+++ DataStructuresAndAlgorithms/ZTMCodeInterview/Tree/BinarySearchTree.cs
@@ -0,0 +1,215 @@
+﻿namespace ZTMCodeInterview.Tree
+{
+    public class BinarySearchTree
+    {
+        public BinarySearchTreeNode Root { get; private set; }
+        public int Count { get; private set; }
+
+        public void Insert(int value)
+        {
+            if (Root == null)
+            {
+                Root = new BinarySearchTreeNode { Value = value };
+            }
+            else
+            {
+                var currentNode = Root;
+
+                while (currentNode != null)
+                {
+                    if (currentNode.Value > value )
+                    {
+                        if(currentNode.Left == null)
+                        {
+                            currentNode.Left = new BinarySearchTreeNode { Value = value };
+                            break;
+                        }                            
+                        currentNode = currentNode.Left;
+                    }
+                    else if (currentNode.Value < value )
+                    {
+                        if(currentNode.Right == null)
+                        {
+                            currentNode.Right = new BinarySearchTreeNode { Value = value };
+                            break;
+                        }                        
+                        currentNode = currentNode.Right;
+                    }
+                }
+            }
+
+            Count++;
+        }
+
+        public bool LookUp(int value)
+        {
+            var currentNode = Root;
+
+            while (currentNode != null)
+            {
+                if(currentNode.Value == value)
+                    return true;
+
+                if (currentNode.Value > value)
+                {
+                   currentNode = currentNode.Left;
+                }
+                else if (currentNode.Value < value)
+                {
+                    currentNode = currentNode.Right;
+                }
+            }
+
+            return false;
+        }
+
+        public (BinarySearchTreeNode found, BinarySearchTreeNode parent) LookUpWithParent(int value)
+        {
+            var currentNode = Root;
+            BinarySearchTreeNode parentNode = null;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Value == value)
+                    return (currentNode, parentNode);
+
+                parentNode = currentNode;
+
+                if (currentNode.Value > value)
+                {
+                    currentNode = currentNode.Left;
+                }
+                else if (currentNode.Value < value)
+                {
+                    currentNode = currentNode.Right;
+                }
+            }
+
+            return (null,null);
+        }
+
+        public void Remove(int value)
+        {
+            var (nodeToDelete, parent) = LookUpWithParent(value);
+
+            if(nodeToDelete != null)
+            {
+                if (nodeToDelete.IsLeaf)
+                {
+                    if (nodeToDelete.Value > parent.Value)
+                        parent.Right = null;
+                    else
+                        parent.Left = null;
+
+                    nodeToDelete = null;
+                    Count--;
+                    return;
+                }
+                //case 1: If the node being deleted has no right child, then the node's left child can be used as the replacement
+                if(nodeToDelete.Right == null)
+                {
+                    if (parent == null)//we wanto to delete the root
+                        Root = nodeToDelete.Left;
+                    else
+                    {
+                        if (nodeToDelete.Left.Value > parent.Value)
+                            parent.Right = nodeToDelete.Left;
+                        else
+                            parent.Left = nodeToDelete.Left;                        
+                    }
+
+                    nodeToDelete = null;
+                    Count--;
+                    return;
+                }
+                //case 2: If the deleted node's right child has no left child, then the deleted node's right child can replace the deleted node
+                if(nodeToDelete.Right.Left == null)
+                {
+                    if (parent == null)
+                        Root = nodeToDelete.Right;
+                    else
+                    {
+                        nodeToDelete.Right.Left = nodeToDelete.Left;
+                        if (nodeToDelete.Right.Value > parent.Value)
+                            parent.Right = nodeToDelete.Right;
+                        else
+                            parent.Left = nodeToDelete.Left;
+                    }
+                    nodeToDelete = null;
+                    Count--;
+                    return;
+                }
+                //case 3: if the deleted node's right child does have a left child, then the deleted node needs to be replaced by the deleted node's right child's left-most descendant
+                if (nodeToDelete.Right.Left != null)
+                {
+                    BinarySearchTreeNode successor = null;
+                    BinarySearchTreeNode successorParent = nodeToDelete.Right;
+
+                    var currentNode = nodeToDelete.Right.Left;
+
+                    while(currentNode != null)
+                    {
+                        if (currentNode.IsLeaf)
+                        {
+                            successor = currentNode;
+                            break;
+                        }                            
+                        else
+                        {
+                            successorParent = currentNode;
+                            currentNode = currentNode.Left;
+                        }                            
+                    }
+
+                    successor.Left = nodeToDelete.Left;
+                    successor.Right = nodeToDelete.Right;
+                    successorParent.Left = null;
+
+                    if (parent == null)
+                        Root = successor;
+                    else
+                    {
+                        if (successor.Value > parent.Value)
+                            parent.Right = successor;
+                        else
+                            parent.Left = successor;
+                    } 
+                    nodeToDelete = null;
+                    Count--;
+                    return;
+                }                
+            }
+        }
+
+        public List<int> ToList()
+        {
+            var list = new List<int>();
+
+            InternalToArray(Root, list);
+
+            return list;
+        }
+
+        private void InternalToArray(BinarySearchTreeNode node, List<int> list)
+        {
+            if (node == null) return;
+            list.Add(node.Value);
+            if (node.Left == null && node.Right == null)
+            {                
+                return;
+            }            
+            InternalToArray(node.Left, list);
+            InternalToArray(node.Right, list);
+        }
+    }
+
+    public class BinarySearchTreeNode
+    {
+        public int Value { get; set; }
+        public BinarySearchTreeNode Left { get; set; }
+        public BinarySearchTreeNode Right { get; set; }
+
+        public bool IsLeaf { get { return Left == null && Right == null; } }
+    }
+}
+ 
---
+++ DataStructuresAndAlgorithms/ZTMCodeInterviewTesting/BinarySearchTreeTests.cs
@@ -0,0 +1,292 @@
+﻿using ZTMCodeInterview.Tree;
+
+namespace ZTMCodeInterviewTesting
+{
+    public class BinarySearchTreeTests
+    {
+        [Fact]
+        public void InsertShouldWork()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+
+            //assert
+            Assert.Equal(1, bs.Count);
+        }
+
+        [Fact]
+        public void InsertShouldWork2()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+
+            //assert
+            Assert.Equal(2, bs.Count);
+        }
+
+        [Fact]
+        public void InsertShouldWork3()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+
+            //assert
+            Assert.Equal(7, bs.Count);
+        }
+
+        [Fact]
+        public void ToArrayShouldWork()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            var list = bs.ToList();
+
+            //assert
+            Assert.Equal(1, bs.Count);
+            Assert.Equal([9], list);
+        }
+
+        [Fact]
+        public void ToArrayShouldWork2()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            var list = bs.ToList();
+
+            //assert
+            Assert.Equal(2, bs.Count);
+            Assert.Equal([9, 4], list);
+        }
+
+        [Fact]
+        public void ToArrayShouldWork3()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            var list = bs.ToList();
+
+            //assert
+            Assert.Equal(7, bs.Count);
+            Assert.Equal([9, 4 , 1, 6, 20, 15, 170], list);
+        }
+
+        [Fact]
+        public void LookUpShouldWork()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            var found = bs.LookUp(4);
+
+            //assert
+            Assert.Equal(2, bs.Count);
+            Assert.True(found);
+        }
+
+        [Fact]
+        public void LookUpShouldWorkNotFound()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            var found = bs.LookUp(7);
+
+            //assert
+            Assert.Equal(2, bs.Count);
+            Assert.False(found);
+        }
+
+        [Fact]
+        public void LookUpShouldWork2()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            var found = bs.LookUp(15);
+
+            //assert
+            Assert.Equal(7, bs.Count);
+            Assert.True(found);
+        }
+
+        [Fact]
+        public void LookUpShouldWorkNotFound2()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            var found = bs.LookUp(100);
+
+            //assert
+            Assert.Equal(7, bs.Count);
+            Assert.False(found);
+        }
+
+        [Fact]
+        public void LookUpWithParentShouldWork()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            var (found,parent) = bs.LookUpWithParent(15);
+
+            //assert
+            Assert.Equal(7, bs.Count);
+            Assert.True(found != null);
+            Assert.Equal(15, found.Value);
+            Assert.Equal(20, parent.Value);
+        }
+
+        [Fact]
+        public void RemoveShouldWorkWhenIsLeaf()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            bs.Remove(15);
+            var list = bs.ToList();
+
+            //assert
+            Assert.Equal(6, bs.Count);
+            Assert.Equal([9, 4, 1, 6, 20, 170], list);
+        }
+
+        [Fact]
+        public void RemoveShouldWorkWhenCase1()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            bs.Insert(150);
+            bs.Remove(170);
+            var list = bs.ToList();
+
+            //assert
+            Assert.Equal(7, bs.Count);
+            Assert.Equal([9, 4, 1, 6, 20, 15, 150], list);
+        }
+
+        [Fact]
+        public void RemoveShouldWorkWhenCase2()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            bs.Remove(20);
+            var list = bs.ToList();
+
+            //assert
+            Assert.Equal(6, bs.Count);
+            Assert.Equal([9, 4, 1, 6, 170, 15], list);
+        }
+
+        [Fact]
+        public void RemoveShouldWorkWhenCase3()
+        {
+            //arrange
+            var bs = new BinarySearchTree();
+
+            //act
+            bs.Insert(9);
+            bs.Insert(4);
+            bs.Insert(6);
+            bs.Insert(20);
+            bs.Insert(170);
+            bs.Insert(15);
+            bs.Insert(1);
+            bs.Insert(150);
+            bs.Remove(20);
+            var list = bs.ToList();
+
+            //assert
+            Assert.Equal(7, bs.Count);
+            Assert.Equal([9, 4, 1, 6, 150 , 15, 170], list);
+        }
+    }
+}
