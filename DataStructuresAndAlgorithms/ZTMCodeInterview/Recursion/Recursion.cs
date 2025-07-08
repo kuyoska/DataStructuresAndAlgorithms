de61797784d2b26da226261dccc8c4a889126304
Carlos Cruz <cruzgarciacarlos87@outlook.com>
2025-07-07 23:33:17 -0600

    recursion problems

---
+++ DataStructuresAndAlgorithms/ZTMCodeInterview/Recursion/Recursion.cs
@@ -0,0 +1,103 @@
+﻿
+namespace ZTMCodeInterview.Recursion
+{
+    public class Recursion
+    {
+        public int Factorial(int number)
+        {
+            if(number == 2)
+                return 2;
+            return number * Factorial(number - 1);
+        }
+
+        public int FactorialIterative(int number)
+        {
+            var factorial = number;
+
+            for (int i = 1; i < number - 1; i++)
+            {
+                factorial *= number - i;
+            }
+
+            return factorial;
+        }
+
+        /// <summary>
+        /// This is O(2^N)
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public int Fibonachi(int number)
+        {
+            if (number < 2) 
+                return number;
+
+            return Fibonachi(number - 1) + Fibonachi(number - 2);
+        }
+
+        public int FibonachiDP(int number)
+        {
+            if (number < 2)
+                return number;
+
+            //f(0) = 0
+            //f(1) = 1
+            //f(n) = f(n - 1) + f(n - 2)
+
+            int before = 0; //F(0)
+            int current = 1; //F(1)
+            int resultado = 0;
+
+            for (int i = 2; i <= number; i++)
+            {
+                resultado = before + current;
+                before = current;
+                current = resultado;
+            }
+
+            return resultado;
+        }
+
+        public int FibonachiIterative(int number)
+        {
+            var listFib = new List<int> { 0, 1};
+
+            for (int i = 2; i <= number; i++)
+            {
+                listFib.Add(listFib[i - 1] + listFib[i - 2]);
+            }
+
+            return listFib[number];
+        }
+
+        public string ReverseStringRecursive(string str)
+        {
+            var reversedString = str.ToCharArray();
+            ReverseStringRecursiveInternal(reversedString, 0, str.Length - 1);
+
+            return new string(reversedString);
+        }
+
+        private void ReverseStringRecursiveInternal(char[] reversedString, int left, int right)
+        {
+            if (left >= right)
+                return;
+            else
+            {
+                var tmp = reversedString[left];
+                reversedString[left] = reversedString[right];
+                reversedString[right] = tmp;
+                ReverseStringRecursiveInternal(reversedString, left + 1, right - 1);
+            }
+        }
+
+        public string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input) || input.Length == 1)
+                return input;
+
+            return ReverseString(input.Substring(1)) + input[0];
+        }
+
+    }
+}
---
+++ DataStructuresAndAlgorithms/ZTMCodeInterviewTesting/RecursionTests.cs
@@ -0,0 +1,98 @@
+﻿using ZTMCodeInterview.Recursion;
+
+namespace ZTMCodeInterviewTesting
+{
+    public class RecursionTests
+    {
+        [Fact]
+        public void FactorialRecursiveShouldWork()
+        {
+            //arrange
+            var rs = new Recursion();
+
+            //act
+            var result = rs.Factorial(5);
+
+            //assert
+            Assert.Equal(120, result);
+        }
+
+        [Fact]
+        public void FactorialIterativeShouldWork()
+        {
+            //arrange
+            var rs = new Recursion();
+
+            //act
+            var result = rs.FactorialIterative(5);
+
+            //assert
+            Assert.Equal(120, result);
+        }
+
+        [Fact]
+        public void FibonachiShouldWork()
+        {
+            //arrange
+            var rs = new Recursion();
+
+            //act
+            var result = rs.Fibonachi(8);
+
+            //assert
+            Assert.Equal(21, result);
+        }
+
+        [Fact]
+        public void FibonachiDPShouldWork()
+        {
+            //arrange
+            var rs = new Recursion();
+
+            //act
+            var result = rs.FibonachiDP(8);
+
+            //assert
+            Assert.Equal(21, result);
+        }
+
+        [Fact]
+        public void FibonachiIterativeShouldWork()
+        {
+            //arrange
+            var rs = new Recursion();
+
+            //act
+            var result = rs.FibonachiIterative(8);
+
+            //assert
+            Assert.Equal(21, result);
+        }
+
+        [Fact]
+        public void ReverseStringRecursiveShouldWork()
+        {
+            //arrange
+            var rs = new Recursion();
+
+            //act
+            var result = rs.ReverseStringRecursive("carlos");
+
+            //assert
+            Assert.Equal("solrac", result);
+        }
+
+        [Fact]
+        public void ReverseString2ShouldWork()
+        {
+            //arrange
+            var rs = new Recursion();
+
+            //act
+            var result = rs.ReverseString("carlos");
+
+            //assert
+            Assert.Equal("solrac", result);
+        }
+    }
+}
